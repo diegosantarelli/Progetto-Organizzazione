1ea7b7940b2dbee891f92a14006c79ec0751143b
SimoneRecinelli <simonerecinelli@gmail.com>
2024-05-14 13:14:06 +0200

    codice corretto, manca la parte dei pdf

--- blockchain.py
+++ blockchain.py
@@ -4,21 +4,27 @@ import requests  # Importa requests per le richieste HTTP
 from bs4 import BeautifulSoup  # Importa BeautifulSoup per il parsing dell'HTML
 
 # Leggi il file Excel
-df = pd.read_excel('Imprese_Agricole_Italia_SitoWeb.xlsx')  # Legge il file Excel in un DataFrame
-websites = list(df['Website'])  # Estrae i siti web dalla colonna 'Website'
-websites = websites[0:200]  # Limita l'analisi ai primi 200 siti web
+dataframe = pd.read_excel('Imprese_Agricole_Italia_SitoWeb.xlsx')  # Legge il file Excel in un DataFrame
+websites = list(dataframe['Website'])  # Estrae i siti web dalla colonna 'Website'
+partite_iva = list(dataframe['Partita IVA'])
+websites = websites[0:100]  # Limita l'analisi ai primi x siti web
+partite_iva = partite_iva[0:100]
 
-def check_website(website) -> (bool, str, int):
+def check_website(partita_iva) -> (bool, str, int):
     """
-    Controlla la presenza della parola "blockchain" nelle pagine del sito web.
+    Funzione che controlla la presenza della parola "blockchain" nelle pagine del sito web.
     
     Args:
         website (str): URL del sito web da controllare.
         
     Returns:
-        Tuple: Una tupla contenente un booleano per la presenza nella homepage,
+        Tuple: Una tupla contenente un booleano per la presenza della parola "blockchain" nella homepage,
         una stringa per l'URL della pagina contenente "blockchain" (se presente), e l'indice del sito nel DataFrame.
     """
+    index = partite_iva.index(partita_iva)
+    website = websites[index]
+    print(index)
+
     try:
         url = f'https://{website}'  # Costruisce l'URL corretto con il protocollo
         response = requests.get(url, timeout=60)  # Effettua una richiesta GET al sito web
@@ -26,12 +32,41 @@ def check_website(website) -> (bool, str, int):
         
         # Controlla se "blockchain" è presente nella home page
         if 'blockchain' in soup.get_text().lower():
-            return True, None, websites.index(website)
+            return True, None, partite_iva.index(partita_iva)
         
-        # Trova tutti i link interni al sito
-        links = list({link.get('href') for link in soup.find_all('a') if
-                      link.get('href') and website in link.get('href') and
-                      link.get('href') not in {f'https://{website}/', f'https://{website}/#'}})
+        # Trova tutti i link interni al sito e li filtra
+        links = {
+        f'https://{link.get("href").rsplit("https://")[-1]}'
+        if 'https://' in link.get('href') else
+        f'http://{link.get("href").rsplit("http://")[-1]}'
+        if 'http://' in link.get('href') else
+        f'https://{website}{link.get("href")}'
+        if (website not in link.get('href') and not link.get('href').startswith('http') and not link.get(
+            'href').startswith('www') and len(link.get('href').split('.')) <= 2) and not website.startswith(
+            'http') else
+        f'{website}{link.get("href")}' if (website not in link.get('href') and not link.get('href').startswith(
+            'http') and not link.get('href').startswith('www') and len(
+            link.get('href').split('.')) <= 2) and website.startswith('http') else
+        link.get('href') for link in soup.find_all('a')
+        if link.get('href') and
+           (website in link.get('href') or (
+                   website not in link.get('href') and not link.get('href').startswith('http') and not link.get(
+               'href').startswith('www') and len(link.get('href').split('.')) <= 2)) and
+           link.get('href') not in {
+               f'https://{website}/',
+               f'https://{website}/#',
+               f'https://{website}',
+               f'http://{website}',
+               f'http://{website}/',
+               f'http://{website}/#'
+           } and 'tel:' not in link.get('href') and 'javascript' not in link.get(
+            'href') and 'mailto:' not in link.get('href') and not link.get(
+            'href').endswith('.mp4') and not link.get('href').endswith('.mp3') and not link.get('href').endswith(
+            '.jpg') and not link.get('href').endswith('.jpeg') and not link.get('href').endswith(
+            '.png') and not link.get('href').endswith('.gif') and not link.get('href').endswith(
+            '.svg') and not link.get('href').endswith('.tiff') and not link.get('href').endswith(
+            '.bmp') and not link.get('href').endswith('.ico') and not link.get('href').endswith('.webp')
+    }
         
         # Controlla ogni link interno per la presenza di "blockchain"
         for link in links:
@@ -40,32 +75,42 @@ def check_website(website) -> (bool, str, int):
             response = requests.get(link, timeout=60)  # Effettua una richiesta GET per il link
             soup = BeautifulSoup(response.text, 'html.parser')  # Parsing dell'HTML
             if 'blockchain' in soup.get_text().lower():
-                return True, link, websites.index(website)  # Ritorna True se "blockchain" è trovata in una pagina
+                return True, link, partite_iva.index(partita_iva)  # Ritorna True se "blockchain" è trovata in una pagina
         
-        return False, None, websites.index(website)  # Ritorna False se "blockchain" non è trovata in nessuna pagina
+        return False, None, partite_iva.index(partita_iva)  # Ritorna False se "blockchain" non è trovata in nessuna pagina
     except Exception as e:
-        print(f'Error checking website {website}: {e}')  # Gestisce eventuali eccezioni
-        return False, None, websites.index(website)  # Ritorna False in caso di errore
+        print(f'Error checking website {website}: {e}')
+        return None, None, partite_iva.index(partita_iva)
 
-# Esegui la verifica per ciascun sito web utilizzando ThreadPoolExecutor
+
+# Cast delle colonne del DataFrame
+"""
+dataframe['blockchain nella Home Page (si/no)'] = dataframe['blockchain nella Home Page (si/no)'].astype(str) 
+dataframe['blockchain in altre pagine (si/no)'] = dataframe['blockchain in altre pagine (si/no)'].astype(str)
+dataframe['Link altre pagine'] = dataframe['Link altre pagine'].astype(str)
+"""
+
+# Utilizza ThreadPoolExecutor per eseguire le richieste HTTP in parallelo
 with ThreadPoolExecutor(max_workers=10) as executor:
-    futures = {executor.submit(check_website, website): website for website in websites}  # Esegue il controllo per ciascun sito web
+    futures = [executor.submit(check_website, partita_iva) for partita_iva in partite_iva]
     for future in as_completed(futures):
-        website = futures[future]  # Ottiene l'URL del sito web dal future
-        result = future.result()  # Ottiene il risultato del controllo
-        # Aggiorna il DataFrame con i risultati del controllo
+        result = future.result()
         if result[0] and result[1] is None:
-            df.at[result[2], 'blockchain nella Home Page (si/no)'] = 'si'
-            df.at[result[2], 'blockchain in altre pagine (si/no)'] = ''
-            df.at[result[2], 'Link altre pagine'] = ''
+            dataframe.at[result[2], 'blockchain nella Home Page (si/no)'] = 'si'
+            dataframe.at[result[2], 'blockchain in altre pagine (si/no)'] = ''
+            dataframe.at[result[2], 'Link altre pagine'] = ''
         elif result[0] and result[1] is not None:
-            df.at[result[2], 'blockchain nella Home Page (si/no)'] = 'no'
-            df.at[result[2], 'blockchain in altre pagine (si/no)'] = 'si'
-            df.at[result[2], 'Link altre pagine'] = result[1]
+            dataframe.at[result[2], 'blockchain nella Home Page (si/no)'] = 'no'
+            dataframe.at[result[2], 'blockchain in altre pagine (si/no)'] = 'si'
+            dataframe.at[result[2], 'Link altre pagine'] = result[1]
+        elif result[0] is None and result[1] is None:
+            dataframe.at[result[2], 'blockchain nella Home Page (si/no)'] = 'errore'
+            dataframe.at[result[2], 'blockchain in altre pagine (si/no)'] = ''
+            dataframe.at[result[2], 'Link altre pagine'] = ''
         else:
-            df.at[result[2], 'blockchain nella Home Page (si/no)'] = 'no'
-            df.at[result[2], 'blockchain in altre pagine (si/no)'] = 'no'
-            df.at[result[2], 'Link altre pagine'] = ''
+            dataframe.at[result[2], 'blockchain nella Home Page (si/no)'] = 'no'
+            dataframe.at[result[2], 'blockchain in altre pagine (si/no)'] = 'no'
+            dataframe.at[result[2], 'Link altre pagine'] = ''
 
 # Salva il DataFrame in un nuovo file Excel
-df.to_excel('Result_Imprese_Agricole_Italia_SitoWeb.xlsx', index=False)  # Salva il DataFrame aggiornato in un nuovo file Excel
+dataframe.to_excel('Result_Imprese_Agricole_Italia_SitoWeb.xlsx', index=False)  # Salva il DataFrame aggiornato in un nuovo file Excel
