47e16048585bfc3810f0baac845fc85324ae2934
SimoneRecinelli <simonerecinelli@gmail.com>
2024-05-18 11:09:57 +0200

    prova

---
+++ Siti_con_blockchain.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# Carica il file Excel
+df = pd.read_excel('Result_Imprese_Agricole_Italia_SitoWeb.xlsx')
+
+# Filtra il DataFrame per visualizzare solo le righe con "si" nella colonna desiderata
+Homepage_con_blockchain = df[df['blockchain nella Home Page (si/no)'] == 'si']
+Link_con_blockchain = df[df['blockchain in altre pagine (si/no)'] == 'si']
+
+# Conta i siti con "si" nella colonna "blockchain nella Home Page (si/no)"
+count_homepage = Homepage_con_blockchain['Website'].count()
+count_link = Link_con_blockchain['Website'].count()
+
+# Visualizza i siti con "si" nella colonna "blockchain nella Home Page (si/no)"
+print("Siti con blockchain nella Home Page:", count_homepage)
+print(Homepage_con_blockchain['Website'])
+
+# Visualizza i siti con "si" nella colonna "blockchain in altre pagine (si/no)"
+print("Siti con blockchain in altre pagine:", count_link)
+print(Link_con_blockchain['Website'])
+
+
+
--- blockchain.py
+++ blockchain.py
@@ -1,15 +1,15 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed  # Importa moduli per esecuzione parallela
-import pandas as pd  # Importa pandas per la manipolazione dei dati
-import requests  # Importa requests per le richieste HTTP
-from bs4 import BeautifulSoup  # Importa BeautifulSoup per il parsing dell'HTML
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 import PyPDF2
 
 # Leggi il file Excel
-dataframe = pd.read_excel('Imprese_Agricole_Italia_SitoWeb.xlsx')  # Legge il file Excel in un DataFrame
-websites = list(dataframe['Website'])  # Estrae i siti web dalla colonna 'Website'
+dataframe = pd.read_excel('Imprese_Agricole_Italia_SitoWeb.xlsx')
+websites = list(dataframe['Website'])
 partite_iva = list(dataframe['Partita IVA'])
-# websites = websites[0:100]  # Limita l'analisi ai primi x siti web
-# partite_iva = partite_iva[0:100]
+websites = websites[0:100]
+partite_iva = partite_iva[0:100]
 
 def search_keyword_in_pdf(url, keyword):
     try:
@@ -29,47 +29,47 @@ def check_website(partita_iva) -> (bool, str, int):
     print(index)
 
     try:
-        url = f'https://{website}'  # Costruisce l'URL corretto con il protocollo
-        response = requests.get(url, timeout=60)  # Effettua una richiesta GET al sito web
-        soup = BeautifulSoup(response.text, 'html.parser')  # Parsing dell'HTML
+        # Tenta la connessione con HTTPS
+        url = f'https://{website}'
+        response = requests.get(url, timeout=60)
+        soup = BeautifulSoup(response.text, 'html.parser')
         
         if 'blockchain' in soup.get_text().lower():
             return True, None, partite_iva.index(partita_iva)
         
         links = {
-        f'https://{link.get("href").rsplit("https://")[-1]}'
-        if 'https://' in link.get('href') else
-        f'http://{link.get("href").rsplit("http://")[-1]}'
-        if 'http://' in link.get('href') else
-        f'https://{website}{link.get("href")}'
-        if (website not in link.get('href') and not link.get('href').startswith('http') and not link.get(
-            'href').startswith('www') and len(link.get('href').split('.')) <= 2) and not website.startswith(
-            'http') else
-        f'{website}{link.get("href")}' if (website not in link.get('href') and not link.get('href').startswith(
-            'http') and not link.get('href').startswith('www') and len(
-            link.get('href').split('.')) <= 2) and website.startswith('http') else
-        link.get('href') for link in soup.find_all('a')
-        if link.get('href') and
-           (website in link.get('href') or (
-                   website not in link.get('href') and not link.get('href').startswith('http') and not link.get(
-               'href').startswith('www') and len(link.get('href').split('.')) <= 2)) and
-           link.get('href') not in {
-               f'https://{website}/',
-               f'https://{website}/#',
-               f'https://{website}',
-               f'http://{website}',
-               f'http://{website}/',
-               f'http://{website}/#'
-           } and 'tel:' not in link.get('href') and 'javascript' not in link.get(
-            'href') and 'mailto:' not in link.get('href') and not link.get(
-            'href').endswith('.mp4') and not link.get('href').endswith('.mp3') and not link.get('href').endswith(
-            '.jpg') and not link.get('href').endswith('.jpeg') and not link.get('href').endswith(
-            '.png') and not link.get('href').endswith('.gif') and not link.get('href').endswith(
-            '.svg') and not link.get('href').endswith('.tiff') and not link.get('href').endswith(
-            '.bmp') and not link.get('href').endswith('.ico') and not link.get('href').endswith('.webp')
-    }
-        
-        # Controlla ogni link interno per la presenza di "blockchain"
+            f'https://{link.get("href").rsplit("https://")[-1]}'
+            if 'https://' in link.get('href') else
+            f'http://{link.get("href").rsplit("http://")[-1]}'
+            if 'http://' in link.get('href') else
+            f'https://{website}{link.get("href")}'
+            if (website not in link.get('href') and not link.get('href').startswith('http') and not link.get(
+                'href').startswith('www') and len(link.get('href').split('.')) <= 2) and not website.startswith(
+                'http') else
+            f'{website}{link.get("href")}' if (website not in link.get('href') and not link.get('href').startswith(
+                'http') and not link.get('href').startswith('www') and len(
+                link.get('href').split('.')) <= 2) and website.startswith('http') else
+            link.get('href') for link in soup.find_all('a')
+            if link.get('href') and
+               (website in link.get('href') or (
+                       website not in link.get('href') and not link.get('href').startswith('http') and not link.get(
+                   'href').startswith('www') and len(link.get('href').split('.')) <= 2)) and
+               link.get('href') not in {
+                   f'https://{website}/',
+                   f'https://{website}/#',
+                   f'https://{website}',
+                   f'http://{website}',
+                   f'http://{website}/',
+                   f'http://{website}/#'
+               } and 'tel:' not in link.get('href') and 'javascript' not in link.get(
+                'href') and 'mailto:' not in link.get('href') and not link.get(
+                'href').endswith('.mp4') and not link.get('href').endswith('.mp3') and not link.get('href').endswith(
+                '.jpg') and not link.get('href').endswith('.jpeg') and not link.get('href').endswith(
+                '.png') and not link.get('href').endswith('.gif') and not link.get('href').endswith(
+                '.svg') and not link.get('href').endswith('.tiff') and not link.get('href').endswith(
+                '.bmp') and not link.get('href').endswith('.ico') and not link.get('href').endswith('.webp')
+        }
+
         for link in links:
             if not link.startswith("http"):
                 link = f'https://{website}{link}'
@@ -83,13 +83,78 @@ def check_website(partita_iva) -> (bool, str, int):
                     return True, link, partite_iva.index(partita_iva)
         
         return False, None, partite_iva.index(partita_iva)
-    except Exception as e:
-        print(f'Error checking website {website}: {e}')
-        return None, None, partite_iva.index(partita_iva)
     
+    except requests.exceptions.RequestException:
+
+        print(f'CONNESSIONE HTTPS FALLITA, ORA PROVA CON HTTP')
+        # Se la connessione HTTPS fallisce, tenta con HTTP
+        try:
+            url = f'http://{website}'
+            response = requests.get(url, timeout=60)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            
+            if 'blockchain' in soup.get_text().lower():
+                return True, None, partite_iva.index(partita_iva)
+            
+            links = {
+                f'https://{link.get("href").rsplit("https://")[-1]}'
+                if 'https://' in link.get('href') else
+                f'http://{link.get("href").rsplit("http://")[-1]}'
+                if 'http://' in link.get('href') else
+                f'https://{website}{link.get("href")}'
+                if (website not in link.get('href') and not link.get('href').startswith('http') and not link.get(
+                    'href').startswith('www') and len(link.get('href').split('.')) <= 2) and not website.startswith(
+                    'http') else
+                f'{website}{link.get("href")}' if (website not in link.get('href') and not link.get('href').startswith(
+                    'http') and not link.get('href').startswith('www') and len(
+                    link.get('href').split('.')) <= 2) and website.startswith('http') else
+                link.get('href') for link in soup.find_all('a')
+                if link.get('href') and
+                   (website in link.get('href') or (
+                           website not in link.get('href') and not link.get('href').startswith('http') and not link.get(
+                       'href').startswith('www') and len(link.get('href').split('.')) <= 2)) and
+                   link.get('href') not in {
+                       f'https://{website}/',
+                       f'https://{website}/#',
+                       f'https://{website}',
+                       f'http://{website}',
+                       f'http://{website}/',
+                       f'http://{website}/#'
+                   } and 'tel:' not in link.get('href') and 'javascript' not in link.get(
+                    'href') and 'mailto:' not in link.get('href') and not link.get(
+                    'href').endswith('.mp4') and not link.get('href').endswith('.mp3') and not link.get('href').endswith(
+                    '.jpg') and not link.get('href').endswith('.jpeg') and not link.get('href').endswith(
+                    '.png') and not link.get('href').endswith('.gif') and not link.get('href').endswith(
+                    '.svg') and not link.get('href').endswith('.tiff') and not link.get('href').endswith(
+                    '.bmp') and not link.get('href').endswith('.ico') and not link.get('href').endswith('.webp')
+            }
+
+            for link in links:
+                if not link.startswith("http"):
+                    link = f'http://{website}{link}'
+                if link.endswith('.pdf'):
+                    if search_keyword_in_pdf(link, 'blockchain'):
+                        return True, link, partite_iva.index(partita_iva)
+                else:
+                    response = requests.get(link, timeout=60)
+                    soup = BeautifulSoup(response.text, 'html.parser')
+                    if 'blockchain' in soup.get_text().lower():
+                        return True, link, partite_iva.index(partita_iva)
+
+            return False, None, partite_iva.index(partita_iva)
+
+        except Exception as e:
+            print(f'NON VA BENE NEANCHE CON HTTP')
+            print(f'Error checking website {website}: {e}')
+            return None, None, partite_iva.index(partita_iva)
+
+# Assicurati che le colonne abbiano il tipo di dato corretto
+dataframe['blockchain nella Home Page (si/no)'] = dataframe['blockchain nella Home Page (si/no)'].astype(str)
+dataframe['blockchain in altre pagine (si/no)'] = dataframe['blockchain in altre pagine (si/no)'].astype(str)
+dataframe['Link altre pagine'] = dataframe['Link altre pagine'].astype(str)
 
 # Utilizza ThreadPoolExecutor per eseguire le richieste HTTP in parallelo
-with ThreadPoolExecutor(max_workers=10) as executor:
+with ThreadPoolExecutor(max_workers=100) as executor:
     futures = [executor.submit(check_website, partita_iva) for partita_iva in partite_iva]
     for future in as_completed(futures):
         result = future.result()
@@ -111,4 +176,4 @@ with ThreadPoolExecutor(max_workers=10) as executor:
             dataframe.at[result[2], 'Link altre pagine'] = ''
 
 # Salva il DataFrame in un nuovo file Excel
-dataframe.to_excel('Result_Imprese_Agricole_Italia_SitoWeb.xlsx', index=False)  # Salva il DataFrame aggiornato in un nuovo file Excel
+dataframe.to_excel('Result_Imprese_Agricole_Italia_SitoWeb.xlsx', index=False)
